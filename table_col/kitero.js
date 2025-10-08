4a83a1d02717a161f7a26aa7c293a88cd4f66a53
Bence <banyai.bence_2022c@bolyai-szakkozep.hu>
2025-10-08 17:14:33 +0200

    kesz a hazi

---
+++ table_col/kitero.js
@@ -0,0 +1,125 @@
+//const { cloneElement } = require("react")
+
+console.log("szar volt a ropdoga")
+const a="szia"
+/**
+ * @type {string}
+ */
+console.log(a)
+
+const b=["a","b","c"]
+console.log(b)
+
+for(let i=0;i<b.length;i++){
+    console.log(b[i])
+}
+
+console.log(b[1])
+
+/**
+ * while()
+ * 
+ * do 
+ * while()
+*/
+
+for(const a of b){
+
+}
+
+for(const key in b){
+    console.log(`${key}: ${b[key]}`)
+}
+/**@type {{name:string, age:number}} */
+const y ={
+    name:"bence",
+    age:17
+}
+
+for(const key in y){
+    console.log(y[key])
+}
+
+ 
+/** 
+ @type {{kor: string, nev: string, sz1: string, sz2?: string}[]}
+ */
+const arr = [
+    {
+        kor: "reformáció",
+        nev: "Balassi Bálint",
+        sz1: "Losonczy Anna",
+        sz2: "Dobó Krisztina"
+    },
+    {
+        kor: "felvilágosodás",
+        nev: "Csokonai",
+        sz1: "Vajda Juliána",
+        
+    },
+    {
+        kor: "magyar romantika",
+        nev: "Petőfi Sándor",
+        sz1: "Mednyánszky Berta",
+        sz2: "Szendrey Júlia"
+    },
+    {
+        kor: "20. század",
+        nev: "Ady Endre",
+        sz1: "Léda",
+        sz2: "Csinszka"
+    }
+]
+
+const table =document.createElement("table")
+document.body.appendChild(table)
+
+const thead =document.createElement("thead")
+table.appendChild(thead)
+
+const tr =document.createElement("tr")
+thead.appendChild(tr)
+
+const th=document.createElement("th")
+tr.appendChild(th)
+
+const th2=document.createElement("th")
+tr.appendChild(th2)
+
+const th3=document.createElement("th")
+tr.appendChild(th3)
+
+th.innerText="Szerző neve"
+th2.innerText="Korszak"
+th3.innerText="Szerelmek"
+th3.colSpan=2
+
+const tbody = document.createElement("tbody")
+table.appendChild(tbody)
+for (const i of arr){
+    const tr = document.createElement("tr")
+    tbody.appendChild(tr)
+
+    const td = document.createElement("td")
+    td.innerText = i.nev
+    tr.appendChild(td)
+
+    const td2 = document.createElement("td")
+    td2.innerText = i.kor
+    tr.appendChild(td2)
+
+    const td3= document.createElement("td")
+    td3.innerText = i.sz1
+    tr.appendChild(td3)
+
+    if(i.sz2 === undefined){
+        td3.colSpan=2
+    }
+    else{
+    const td4 = document.createElement("td")
+    td4.innerText = i.sz2
+    tr.appendChild(td4)
+    }
+}
+
+
---
+++ table_col_hf/index.js
@@ -0,0 +1,75 @@
+/** @type {{theme: string, time:string, scientist1: string, scientist2: string}[]} */
+const arr = [
+    {
+        theme: 'Optika',
+        time: 'XI. szazad',
+        scientist1: 'Alhazen'
+    },
+    {
+        theme: 'Asztronómia',
+        time: 'reneszánsz',
+        scientist1: 'Kepler',
+        scientist2: 'Galilei'
+    },
+    {
+        theme: 'Kvantumfizika',
+        time: 'XIX-XX. század',
+        scientist1: 'Max Planck',
+        scientist2: 'Albert Einstein'
+    },
+    {
+        theme: 'Modern fizika',
+        time: 'XX-XXI. század',
+        scientist1: 'Richard Feynman',
+        scientist2: 'Stephen Hawking'
+    }
+]
+
+/** document.createelement(string)
+ */
+const table =document.createElement("table")
+
+/** document.body.appendChild(htmlelement)
+ */
+document.body.appendChild(table)
+
+const thead =document.createElement("thead")
+table.appendChild(thead)
+const tr =document.createElement("tr")
+thead.appendChild(tr)
+
+const th=document.createElement("th")
+tr.appendChild(th)
+const th2=document.createElement("th")
+tr.appendChild(th2)
+const th3=document.createElement("th")
+tr.appendChild(th3)
+
+th.innerText="Fizika területe"
+th2.innerText="Időszak"
+th3.innerText="Képviselők"
+th3.colSpan=2
+
+const tbody = document.createElement("tbody")
+table.appendChild(tbody)
+for(const i of arr){
+    const tr2 =document.createElement("tr")
+    tbody.appendChild(tr2)
+    const td =document.createElement("td")
+    td.innerText=i.theme
+    tr2.appendChild(td)
+    const td2 =document.createElement("td")
+    td2.innerText=i.time
+    tr2.appendChild(td2)
+    const td3 =document.createElement("td")
+    td3.innerText=i.scientist1
+    tr2.appendChild(td3)
+    if(i.scientist2===undefined){
+        td3.colSpan="2"
+    }
+    else{
+        const td4 =document.createElement("td")
+        td4.innerText=i.scientist2
+        tr2.appendChild(td4)
+    }
+}
