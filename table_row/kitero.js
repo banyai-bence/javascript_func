9a662c4d026766f364ffe50a240690dae7e18456
Banyai Bence <banyai.bence@BOLYAI.LOCAL>
2025-11-07 12:24:37 +0100

    orai esemenykezeles

--- table_row/kitero.js
+++ table_row/kitero.js
@@ -12,102 +12,134 @@ console.log(a)
 console.log(a["name"])
 console.log(a.name)
 
-/** @type {{nemz: string, szerzo: string, mu: string}[]} */
-const arr= [
+/**@type {{nemzet:string,szerzo:string,mu:string,szerzo?:string,mu2?:string}[]} */
+const arr =[
     {
-        nemz: "Orosz",
-        szerzo: "Gogol",
-        mu: "A köpönyeg",
+        nemzet:'Orosz',
+        szerzo:'Gogol',
+        mu:'A köpönyeg',
+        szerzo2:'Csehov',
+        mu2:'A csinovnyik halála',
     },
     {
-        szerzo: "Csehov",
-        mu: "A csinovnyik halála",
+        nemzet:'Cseh',
+        szerzo:'Franz Kafka',
+        mu:'Az átváltozás',
     },
     {
-        nemz: "Cseh",
-        szerzo: "Franz Kafka",
-        mu: "Az átváltozás",
+        nemzet:'Magyar',
+        szerzo:'Örkény István',
+        mu:'Egyperces Novellák',
+        szerzo2:'József Attila',
+        mu2:'Klárisok',
     },
     {
-        nemz: "Magyar",
-        szerzo: "Örkény István",
-        mu: "Egyperces Novellák",
-    },
-    {
-        szerzo: "József Attila",
-        mu: "Klárisok",
-    },
-    {
-        nemz: "Svájc",
-        szerzo: "Friedrich Dürrenmatt",
-        mu: "A fizikusok",
-    }
+        nemzet:'Svájc',
+        szerzo:'Friedrich Dürrenmatt',
+        mu:'A fizikusok',
+    }      
 ]
-
-const table=document.createElement("table")
+ 
+const table = document.createElement('table')
 document.body.appendChild(table)
-
-const thead =document.createElement("thead")
+ 
+const thead = document.createElement('thead')
 table.appendChild(thead)
-
-const tr = document.createElement("tr")
-thead.appendChild(tr)
-
-const c=["Nemzetiség","Szerző","Mű"]
-for(const i of c){
-    const th = document.createElement("th")
+ 
+const trh = document.createElement('tr')
+thead.appendChild(trh)
+ 
+const tomb=["Nemzetiség","Szerző","Mű"]
+for(const i of tomb){
+    const th = document.createElement('th')
     th.innerText=i
-    tr.appendChild(th)
+    thead.appendChild(th)
 }
-
-const tbody = document.createElement("tbody")
+ 
+const tbody = document.createElement('tbody')
 table.appendChild(tbody)
+ 
 
-for(const j of arr){
-    const tr = document.createElement("tr")
-    tbody.appendChild(tr)
+for(const k of arr){
+   
+    const trd = document.createElement('tr')
+    tbody.appendChild(trd)  
+    
+    const tdN = document.createElement('td')
+    tdN.innerText=k.nemzet
+    trd.appendChild(tdN)
+ 
+    tdN.addEventListener('click',function (e){
+        /**@type {HTMLTableCellElement} */
+        const target=e.target
+        target.classList.add('marked')
+ 
+    })
+ 
+    
+    const tdSz = document.createElement('td')
+    tdSz.innerText=k.szerzo
+    trd.appendChild(tdSz)
+    
+    const tdM = document.createElement('td')
+    tdM.innerText=k.mu
+    trd.appendChild(tdM)
+    
+    if(k.szerzo2 !== undefined && k.mu2 !== undefined){
+        tdN.rowSpan=2
+ 
+        const tr = document.createElement('tr')
+        tbody.appendChild(tr)
+ 
+        const tdSz2 = document.createElement('td')
+        tdSz2.innerText=k.szerzo2
+        tr.appendChild(tdSz2)
 
-    if(j.nemz == "Magyar" || j.nemz == "Orosz"){
-
-        const td = document.createElement("td")
-        td.innerText=j.nemz
-        tr.appendChild(td)
-        td.rowSpan=2
-
-        const td2 = document.createElement("td")
-        td2.innerText=j.szerzo
-        tr.appendChild(td2)
-
-        const td3 = document.createElement("td")
-        td3.innerText=j.mu
-        tr.appendChild(td3)
-
-    }
-    else if(j.nemz == "Cseh" || j.nemz == "Svájc"){
-
-        const td = document.createElement("td")
-        td.innerText=j.nemz
-        tr.appendChild(td)
-
-        const td2 = document.createElement("td")
-        td2.innerText=j.szerzo
-        tr.appendChild(td2)
-
-        const td3 = document.createElement("td")
-        td3.innerText=j.mu
-        tr.appendChild(td3)
-
-    }
-    else{
-        const td2 = document.createElement("td")
-        td2.innerText=j.szerzo
-        tr.appendChild(td2)
-
-        const td3 = document.createElement("td")
-        td3.innerText=j.mu
-        tr.appendChild(td3)
+        const tdM2 = document.createElement('td')
+        tdM2.innerText=k.mu2
+        tr.appendChild(tdM2)
     }
 }
 
+/**
+ * @type {HTMLFormElement}
+ */
+const htmlform=document.getElementById("htmlform")
+addEventListener("submit", 
+    function (e){
+        e.preventDefault()
 
+        /** @type {HTMLFormElement} */
+        const targetdefault= e.target
+        
+        /** @type {HTMLInputElement} */
+        const nemz=targetdefault.querySelector("#nemzetiseg")
+        /** @type {string} */
+        const n=nemz.value
 
+        /** @type {HTMLInputElement} */
+        const sz1=targetdefault.querySelector("#szerzo1")
+        /** @type {string} */
+        const s=sz1.value
+
+        /** @type {HTMLInputElement} */
+        const mu1=targetdefault.querySelector("#mu1")
+        /** @type {string} */
+        const m=mu1.value
+
+        /** @type {HTMLInputElement} */
+        const sz2=targetdefault.querySelector("#szerzo2")
+        /** @type {string} */
+        const sz=sz2.value
+
+        /** @type {HTMLInputElement} */
+        const mu2=targetdefault.querySelector("#mu2")
+        /** @type {string} */
+        const mu=mu2.value
+
+        /**@type {{nemzet:string,szerzo:string,mu:string,szerzo?:string,mu2?:string}} */
+        const obj={
+            
+        }
+    }
+)
